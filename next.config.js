929ebdefd1e61c9f5e3b905a850c6fed3a1c87f8
truongson009385 <94103961truongson009385@users.noreply.github.com>
2025-03-14 03:44:09 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   webpack(config) {
     config.module.rules.push({
@@ -11,4 +10,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig; 
